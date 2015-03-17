3b87942a6a288760ecf30bfba0eff4955cb08a75
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-17 15:40:02 -0700

    Wrote the functions for each error

--- students/KimNighelli/session01/break_me.py
+++ students/KimNighelli/session01/break_me.py
@@ -1,4 +1,16 @@
 import datetime
 #I use datetime all the time at work to deal with date format and timezone issues.
 
+def syntax_error(some_date, some_fmt):
+    return datetime.strptime()
+
+def attribute_error(some_date, some_fmt):
+    return datetime.strpptime()
+
+def type_error(string, some_float):
+    return string + some_float
+
+def name_error():
+    return x
+
 
