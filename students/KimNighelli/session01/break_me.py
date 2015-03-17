80f682c6c6f527eb4762ae9673fcad769208ee6c
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-17 15:43:31 -0700

    Wrote the syntax error example

--- students/KimNighelli/session01/break_me.py
+++ students/KimNighelli/session01/break_me.py
@@ -13,4 +13,13 @@ def type_error(string, some_float):
 def name_error():
     return x
 
+#Syntax Error
+# A syntax error occurs when Python syntax is incorrect. The syntax
+# below is incorrect in the fmt block, there should not be three quotes.
+syntax_error("2015-03-16", "%Y-"%m-%d")
 
+#Error Received:
+#  File "break_me.py", line 19
+#    syntax_error("2015-03-16", "%Y-"%m-%d")
+#                                       ^
+#SyntaxError: invalid syntax
