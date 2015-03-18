f4a3378cc71f223ce970337c64ef2f1c423a5806
Karen Wong <karen.wsywong@gmail.com>
2015-03-18 09:20:54 -0700

    added session 1 folder

---
+++ students/KarenWong/session 01/break_me.py
@@ -0,0 +1,21 @@
+def name_error_function():
+    hello()
+
+
+def type_error_function():
+    x = 1
+    x()
+
+
+def syntax_error_function(s):
+    print("Karen")
+
+
+def attribute_error_function():
+    "hello".not_a_function()
+
+
+#name_error_function()
+#type_error_function()
+#syntax_error_function()
+#attribute_error_function()
