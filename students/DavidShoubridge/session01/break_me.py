895dd26ab545ed2d37ba2b21a884e8e5877d2df8
davidmshou <david.m.shou@gmail.com>
2015-03-18 08:37:39 -0700

    Adds name error, syntax error, and type error

---
+++ students/DavidShoubridge/session01/break_me.py
@@ -0,0 +1,19 @@
+def syntax-error(x, y):
+    print(x + y)
+
+
+def name_error(name):
+    print name
+
+
+x = 2
+y = "3"
+
+
+def type_error(x, y):
+    print x + y
+
+
+syntax-error(2, 2)
+nameError(name)
+typeError(x, y)
