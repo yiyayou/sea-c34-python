db5bb77c982a2fb5f64b9c9331d4492273bfbe46
Scott Schmidt <sjschmidt44@gmail.com>
2015-03-19 12:03:34 -0700

    Correct minor discrepency in Fib Sequence function to mirror functionality with Lucas Sequence.

--- students/ScottSchmidt/series.py
+++ students/ScottSchmidt/series.py
@@ -1,22 +1,35 @@
-""" Return the nth number of the Fibonacci sequence from user input.
+""" Return the nth number of the Fibonacci sequence.
 
     Arg:
-        user: Provide user input, Integer value, from the command line.
+        user: Provide fibUser input, Integer value.
 
     Return:
         Return single integer value of Fibonacci sequence at index,
-        determined by user input. """
-
-fibUser = int(raw_input("nth number of Fib: "))
+        determined by function argument. """
 
 
 def fib(fibUser):
-    x, y, i = 0, 1, 1
-    while i < fibUser:
-        x, y, i = x+y, x, i+1
-    return x
+    first, second, count = 0, 1, 1
+    while count < fibUser:
+        first, second, count = second, first + second, count+1
+    return first
 
-print fib(fibUser)
+
+""" Return the nth number of the Lucas sequence.
+
+    Arg:
+        user: Provide user input, Integer value.
+
+    Return:
+        Return single integer value of Lucas sequence at index,
+        determined by function argument. """
+
+
+def lucas(lucUser):
+    first, second, count = 2, 1, 1
+    while count < lucUser:
+        first, second, count = second, first + second, count + 1
+    return first
 
 
 """ Return the nth number of the Lucas sequence from user input.
@@ -28,13 +41,27 @@ print fib(fibUser)
         Return single integer value of Lucas sequence at index,
         determined by user input. """
 
-lucUser = int(raw_input("nth number of Luc: "))
+seriesUser = int(raw_input("nth number of Luc: "))
 
 
-def lucas(lucUser):
-    first, second, count = 2, 1, 1
-    while count < lucUser:
-        first, second, count = second, first + second, count + 1
-    return first
+def sum_series():
 
-print lucas(lucUser)
+
+
+
+
+"""Both the *fibonacci series* and the *lucas numbers* are based on an
+identical formula.
+
+Add a third function called ``sum_series`` with one required parameter
+and two optional parameters. The required parameter will determine which
+element in the series to print. The two optional parameters will have
+default values of 0 and 1 and will determine the first two values for
+the series to be produced.
+
+Calling this function with no optional paramters will produce numbers
+from the *fibonacci series*. Calling it with the optional arguments 2
+and 1 will produce values from the *lucas numbers*. Other values for the
+optional parameters will produce other series.
+
+Ensure that your function has a well-formed ``docstring``"""
