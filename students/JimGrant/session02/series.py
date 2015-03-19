22d894f481a0980b7bc1777bd843c1b965f57144
Jim Grant <jimgrant@gmail.com>
2015-03-19 15:29:59 -0700

    add sum_series function

--- students/JimGrant/session02/series.py
+++ students/JimGrant/session02/series.py
@@ -16,3 +16,16 @@ def lucas(n):
         value1, value2 = value2, value1 + value2
 
     return value1
+
+
+def sum_series(n, value1=0, value2=1):
+    """Return the nth number in a recursive integer sequence.
+    Args:
+        value1 - The first number in the sequence to use.
+        value2 = The second number in the sequence to use.
+    """
+
+    for i in range(n - 1):
+        value1, value2 = value2, value1 + value2
+
+    return value1
