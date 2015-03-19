5f1d6ee1a0f432db80b7747dcfb42ae34c3b48e2
Scott Schmidt <sjschmidt44@gmail.com>
2015-03-19 16:38:49 -0700

    Add series_sum function and assert tests to confirm functionality

--- students/ScottSchmidt/series.py
+++ students/ScottSchmidt/series.py
@@ -1,67 +1,69 @@
-""" Return the nth number of the Fibonacci sequence.
+def fib(fibIndex):
+    """ Return the nth number of the Fibonacci sequence.
 
-    Arg:
-        user: Provide fibUser input, Integer value.
+        Arg:
+            fibIndex: Provide Integer value argument for nth index of
+            Fibonacci series.
 
-    Return:
-        Return single integer value of Fibonacci sequence at index,
-        determined by function argument. """
-
-
-def fib(fibUser):
+        Return:
+            Return single integer value of Fibonacci sequence at index,
+            determined by function argument. """
     first, second, count = 0, 1, 1
-    while count < fibUser:
+    while count < fibIndex:
         first, second, count = second, first + second, count+1
     return first
 
 
-""" Return the nth number of the Lucas sequence.
+def lucas(lucIndex):
+    """ Return the nth number of the Lucas sequence.
 
-    Arg:
-        user: Provide user input, Integer value.
+        Arg:
+            lucIndex: Provide Integer value argument for nth index of Lucas
+            series.
 
-    Return:
-        Return single integer value of Lucas sequence at index,
-        determined by function argument. """
-
-
-def lucas(lucUser):
+        Return:
+            Return single integer value of Lucas sequence at index,
+            determined by function argument. """
     first, second, count = 2, 1, 1
-    while count < lucUser:
+    while count < lucIndex:
         first, second, count = second, first + second, count + 1
     return first
 
 
-""" Return the nth number of the Lucas sequence from user input.
+def sum_series(nth, first=0, second=1):
+    """ Return the nth number of the Lucas sequence from user input.
 
-    Arg:
-        user: Provide user input, Integer value, from the command line.
+        Arg:
+            nth: Provide Integer value argument for nth index of Lucas
+            series.
 
-    Return:
-        Return single integer value of Lucas sequence at index,
-        determined by user input. """
+            first: Optional parameter argument, determine the beginning
+            integer value of the series.
 
-seriesUser = int(raw_input("nth number of Luc: "))
-
-
-def sum_series():
-
+            second: Optional parameter argument, determine the second
+            integer value of the series.
 
 
+        Return:
+            Return single integer value of the sequence at index,
+            determined by user input, and use optional argument values for
+            first and second index values of series. """
+    count = 1
+    while count < nth:
+        first, second, count = second, first + second, count + 1
+    return first
 
 
-"""Both the *fibonacci series* and the *lucas numbers* are based on an
-identical formula.
+if __name__ == "__main__":
+    """Test output values for each given index argument."""
+    assert fib(10) == 34
+    assert fib(11) == 55
+    assert fib(12) == 89
 
-Add a third function called ``sum_series`` with one required parameter
-and two optional parameters. The required parameter will determine which
-element in the series to print. The two optional parameters will have
-default values of 0 and 1 and will determine the first two values for
-the series to be produced.
+    assert lucas(10) == 76
+    assert lucas(11) == 123
+    assert lucas(12) == 199
 
-Calling this function with no optional paramters will produce numbers
-from the *fibonacci series*. Calling it with the optional arguments 2
-and 1 will produce values from the *lucas numbers*. Other values for the
-optional parameters will produce other series.
-
-Ensure that your function has a well-formed ``docstring``"""
+    assert sum_series(10) == 34
+    assert sum_series(11, 2, 5) == 343
+    assert sum_series(12, 4, 3) == 487
