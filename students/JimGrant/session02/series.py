28bd4f041a2c92b1dd462f9ad3b8d4104d4f85f0
Jim Grant <jimgrant@gmail.com>
2015-03-19 14:51:31 -0700

    add lucas function

--- students/JimGrant/session02/series.py
+++ students/JimGrant/session02/series.py
@@ -6,3 +6,13 @@ def fibonacci(n):
         value1, value2 = value2, value1 + value2
 
     return value1
+
+
+def lucas(n):
+    """Return the nth number in the Lucas Numbers."""
+
+    value1, value2 = 2, 1
+    for i in range(n - 1):
+        value1, value2 = value2, value1 + value2
+
+    return value1
