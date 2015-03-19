2f0a3d9c2085d1fc38a9b4ab2de89af4345e1fb7
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-19 13:45:38 -0700

    Made the docstrings

--- students/KimNighelli/session02/series.py
+++ students/KimNighelli/session02/series.py
@@ -2,12 +2,14 @@
 # This is for Task 4- Mathematical Series
 ############################################
 
-# Fibonacci Series
-# 0, 1, 1, 2, 3, 5, 8, 13, ...
-# The function for a Fibonnaci series is F(n) = F(n-1) + F(n-2)
-# Where n is the nth number in the series.
-# What we know- F(0) = 0, F(1) = 1, F(2) = 1
-# For this assignment, we calculate the nth number in the series
+'''
+Fibonacci Series
+0, 1, 1, 2, 3, 5, 8, 13, ...
+The function for a Fibonnaci series is F(n) = F(n-1) + F(n-2)
+Where n is the nth number in the series.
+What we know- F(0) = 0, F(1) = 1, F(2) = 1
+For this assignment, we calculate the nth number in the series
+'''
 
 def fibonacci(n):
         if n < 0:
@@ -19,12 +21,13 @@ def fibonacci(n):
         else:
                 return fibonacci(n-1) + fibonacci(n-2)
 
-# Lucas Series
-# 2, 1, 3, 4, 7, 11, 18, 29, ...
-# The function for the Lucas number is the same as Fibonacci,
-# except that the first number is a 2 instead of the 0,1 pair. 
-# For this assignment, we calculate the nth number in the series
-
+'''
+Lucas Series
+2, 1, 3, 4, 7, 11, 18, 29, ...
+The function for the Lucas number is the same as Fibonacci,
+except that the first number is a 2 instead of the 0,1 pair. 
+For this assignment, we calculate the nth number in the series
+'''
 
 def lucas(n):
         if n < 0:
@@ -37,12 +40,14 @@ def lucas(n):
                 return lucas(n-1) + lucas (n-2)
 
 
-# Sum Series
-# Need- One required (user) and two optional parameters
-# The required parameter will determine which element in the series to print. 
-# The two optional parameters will have default values of 0 and 1 and will 
-# determine the first two values for the series to be produced.
-### To deal with this, make two variables - y = 0, z = 1
+'''
+Sum Series
+Need- One required (user) and two optional parameters
+The required parameter will determine which element in the series to print. 
+The two optional parameters will have default values of 0 and 1 and will 
+determine the first two values for the series to be produced.
+To deal with this, make two variables - y = 0, z = 1
+'''
 
 def sum_series (n, y = 0, z = 1):
         if n < 0:
