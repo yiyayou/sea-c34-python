f8a65324d221ea3e0fb207a1c9e20e7b630f9a38
Jonathan Stallings <jonathanstallings@outlook.com>
2015-03-18 18:20:52 -0700

    Update break_me.py with functions.

---
+++ students/JonathanStallings/break_me.py
@@ -0,0 +1,20 @@
+def name_break():
+    return broken
+
+
+def type_break():
+    a = 'broken'
+    return a/5
+
+
+def syntax_break():
+    eval("")
+
+
+def attribute_break():
+    foo = {}
+    foo.broken
+# name_break()
+# type_break()
+# syntax_break()
+# attribute_break()
