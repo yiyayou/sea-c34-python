5fe079beedf4d8cf0afc1be7871ba0db8070118b
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-18 19:07:18 -0700

    Wrote the beginnings of Lucas

--- students/KimNighelli/session02/series.py
+++ students/KimNighelli/session02/series.py
@@ -14,10 +14,27 @@ def fibonacci(n):
                 return None
         if n == 0:
                 return 0
-        if n == 1:
+        elif n == 1:
                 return 1
-        if n > 1:
+        else:
                 return fibonacci(n-1) + fibonacci(n-2)
+
+# Lucas Series
+# 2, 1, 3, 4, 7, 11, 18, 29, ...
+# The function for the Lucas number is the same as Fibonacci,
+# except that the first number is a 2 instead of the 0,1 pair. 
+# For this assignment, we calculate the nth number in the series
+
+
+def lucas(n):
+        if n < 0:
+                return None
+        if n == 0:
+                return 2
+        elif n == 1:
+                return 1
+        else:
+                return lucas(n-1) + lucas (n-2)
 
 
 print fibonacci(0)
