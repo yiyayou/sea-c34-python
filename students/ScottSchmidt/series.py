7a2260ef89882342d704491bdafeb1e8daf4c80e
Scott Schmidt <sjschmidt44@gmail.com>
2015-03-19 11:26:48 -0700

    Add series.py to repository, with first task complete (Fib Sequence).

--- students/ScottSchmidt/series.py
+++ students/ScottSchmidt/series.py
@@ -1,5 +1,40 @@
-def fibonacci(x):
-    fib = []
-    for num in range(0):
+""" Return the nth number of the Fibonacci sequence from user input.
 
-        fib.append(object)
+    Arg:
+        user: Provide user input, Integer value, from the command line.
+
+    Return:
+        Return single integer value of Fibonacci sequence at index,
+        determined by user input. """
+
+user = int(raw_input("nth number of Fib: "))
+
+
+def fib(user):
+    x, y, i = 0, 1, 1
+    while i < user:
+        x, y, i = x+y, x, i+1
+    return x
+
+print fib(user)
+
+
+""" Return the nth number of the Fibonacci sequence from user input.
+
+    Arg:
+        user: Provide user input, Integer value, from the command line.
+
+    Return:
+        Return single integer value of Fibonacci sequence at index,
+        determined by user input. """
+
+user = int(raw_input("nth number of Fib: "))
+
+
+def fib(user):
+    x, y, i = 0, 1, 1
+    while i < user:
+        x, y, i = x+y, x, i+1
+    return x
+
+print fib(user)
