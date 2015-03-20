e1b66398870ae1247212538e426a6ba7347de70f
jonathanwoodard <jwoodard@gmail.com>
2015-03-19 22:06:17 -0700

    create attribute error in break_me.py

--- students/JonWoodard/session01/break_me.py
+++ students/JonWoodard/session01/break_me.py
@@ -1,4 +1,9 @@
 #creating module with four errors
-print("Hello, world!")
-print("Hello, world!", + 42)
-
+#how to tell if she is a whitch?
+whitch="Burn her!"
+#resolve name error by defining *whitch*
+#she=whitch()  this creates a type error - str object not callable
+she=[]    #type error resolved
+setattr(she, 'turned_me_into_a_newt', True)    #this creates attribute error
+#list object has no attribute 'turned_me_into_a_newt'
+print(she)
