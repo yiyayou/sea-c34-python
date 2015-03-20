4b47e20c3e18c6c1583a6a6beb25a872e40ec983
Jim Grant <jimgrant@gmail.com>
2015-03-19 17:14:23 -0700

    make series start counting at 0

--- students/JimGrant/session02/series.py
+++ students/JimGrant/session02/series.py
@@ -5,7 +5,7 @@ def fibonacci(n):
     """
 
     value1, value2 = 0, 1
-    for i in range(n - 1):
+    for i in range(n):
         value1, value2 = value2, value1 + value2
 
     return value1
@@ -18,7 +18,7 @@ def lucas(n):
     """
 
     value1, value2 = 2, 1
-    for i in range(n - 1):
+    for i in range(n):
         value1, value2 = value2, value1 + value2
 
     return value1
@@ -32,7 +32,7 @@ def sum_series(n, value1=0, value2=1):
         value2 - int (optional, default 1). The second number in the sequence to use.
     """
 
-    for i in range(n - 1):
+    for i in range(n):
         value1, value2 = value2, value1 + value2
 
     return value1
@@ -40,17 +40,17 @@ def sum_series(n, value1=0, value2=1):
 
 if __name__ == "__main__":
     # test that the internally supplied numbers in the series are returned properly
-    assert fibonacci(1) == 0
-    assert fibonacci(2) == 1
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
 
     # test a large-ish n for the correct number in the series
-    assert fibonacci(10) == 34
-    assert lucas(10) == 76
+    assert fibonacci(9) == 34
+    assert lucas(9) == 76
 
     # test the default values act as a fibonacci series
-    assert sum_series(10) == 34
+    assert sum_series(9) == 34
 
     # test a couple different series with large-ish n
-    assert sum_series(10, 2, 1) == 76
-    assert sum_series(10, 2, 4) == 178
+    assert sum_series(9, 2, 1) == 76
+    assert sum_series(9, 2, 4) == 178
     print("Tests completed successfully!")
