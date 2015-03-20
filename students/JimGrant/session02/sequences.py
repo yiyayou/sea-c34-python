c8152e783e996f6dbb1bb177d283bf9ae172846d
Jim Grant <jimgrant@gmail.com>
2015-03-19 20:35:30 -0700

    finish question03 function. improve readability of other functions and docstrings

--- students/JimGrant/session02/sequences.py
+++ students/JimGrant/session02/sequences.py
@@ -2,7 +2,7 @@ import sys
 
 
 def question01():
-    """Answers the question: what happens if the end of my slice is greater than the beginning?"""
+    """Answer the question: what happens if the end of my slice is greater than the beginning?"""
     print("Question 1: What happens if the end of my slice is greater than the beginning?")
 
     testlist = [1, 2, 3]
@@ -12,11 +12,12 @@ def question01():
 
 
 def question02():
-    """Answers the question: what happens if I try to use a float for a list index?"""
+    """Answer the question: what happens if I try to use a float for a list index?"""
     print("Question 2: What happens if I try to use a float for a list index?")
 
     testlist = [1, 2, 3]
     testindex = 0.5
+    
     try:
         print("Python thinks the {}th index of {} is: {}".format(testindex, testlist, testlist[0.5]))
     except:
@@ -26,7 +27,18 @@ def question02():
 
 
 def question03():
-    pass
+    """Answer the question: can I use list() to make a deep copy of a list?"""
+    print("Question 3: Can I use list() to make a deep copy of a list?")
+
+    testlist = [1, 2, 3, ["a", "b"]]
+    testlist_copy = list(testlist)
+    testlist[-1].pop()
+
+    # if testlist_copy is shallow, "b" should have gotten popped off both lists
+    if testlist == testlist_copy:
+        print("No, list() creates a shallow copy of a provided list, just like [:]")
+    else:
+        print("Yes, list() creates a deep copy of a provided list")
 
 
 if __name__ == "__main__":
