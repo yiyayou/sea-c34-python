11e34443d908bd34ea1ab27ecdd32bfd005b47ce
Jim Grant <jimgrant@gmail.com>
2015-03-19 20:04:19 -0700

    finish question01 function

--- students/JimGrant/session02/sequences.py
+++ students/JimGrant/session02/sequences.py
@@ -1,5 +1,11 @@
 def question01():
-    pass
+    """Answers the question: what happens if the end of my slice is greater than the beginning?"""
+    print("Question 1: What happens if the end of my slice is greater than the beginning?")
+
+    testlist = [1, 2, 3]
+    upper_bound = len(testlist) - 1
+    print("{} sliced from 0 to {} is: {}".format(testlist, upper_bound, testlist[0:upper_bound]))
+    print("However, {} sliced from {} to 0 is: {}".format(testlist, upper_bound, testlist[upper_bound:0]))
 
 
 def question02():
