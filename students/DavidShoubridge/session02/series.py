7ba8910e5a3255d93d006d5aa609ab9ec083358e
davidmshou <david.m.shou@gmail.com>
2015-03-19 17:58:09 -0700

    Add sum_series that calls fib or lucas

--- students/DavidShoubridge/session02/series.py
+++ students/DavidShoubridge/session02/series.py
@@ -1,15 +1,19 @@
+x = 0
+y = 1
+
+
 def fibonacci(n):
     """
-    Return the nth number in Fibonacci Series.
+    Return the nth number in the Fibonacci Series.
 
     Arg:
-        n(int): Any number.
+        n(int): Element within the Fibonacci series.
     Return:
-        The nth value in the Fibonacci Series that corresponds to the value n.
+        The value in the Fibonacci Series that corresponds to the value of n.
     """
-    if n == 0:
+    if n == x:
         return 0
-    elif n == 1:
+    elif n == y:
         return 1
     else:
         series = [0, 1]
@@ -22,29 +26,37 @@ print fibonacci(4)
 
 def lucas(n):
     """
-    Return the nth number in Lucas Series.
+    Return the nth number in the Lucas Series.
 
     Arg:
-        n(int): Any number.
+        n(int): Index within the Lucas series.
     Return:
-        The nth value in the Lucas Series that corresponds to the value n.
+        The value in the Lucas Series that corresponds to the value of n.
     """
-    if n == 0:
+    if n == x:
         return 2
-    elif n == 1:
+    elif n == y:
         return 1
     else:
-        return (n - 1) + (n - 2)
+        series = [2, 1]
+        for i in range(n - 1):
+            series.append(series[i] + series[i + 1])
+        return series[len(series) - 1]
+
+print lucas(4)
 
 
 def sum_series(n, x=0, y=1):
     """
-    Return numbers from either Fibonacci Series or Lucas Series depending on arguments.
+    Return numbers from either Fibonacci Series or Lucas Series depending on \
+    arguments.
 
     Args:
         n(int): Determines which element in the series to print.
-        x(int): Optional standard argument that will return numbers from Fibonacci Series if used.
-        y(int): Optional standard argument that will return numbers from the Fibonacci Series if used.
+        x(int): Optional standard argument that will return numbers from \
+        Fibonacci Series if used.
+        y(int): Optional standard argument that will return numbers from the \
+        Fibonacci Series if used.
     """
 
     if (x == 0) and (y == 1):
@@ -52,4 +64,4 @@ def sum_series(n, x=0, y=1):
     elif (x == 2) and (y == 1):
         return lucas(n)
 
-print sum_series(3)
+print sum_series(5)
