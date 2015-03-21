9eebef78fa2a17ded1d054fc8348422ec917013d
Scott Schmidt <sjschmidt44@gmail.com>
2015-03-21 13:03:47 -0700

    Add testing to verify ack function works correctly.

--- students/ScottSchmidt/ack.py
+++ students/ScottSchmidt/ack.py
@@ -3,14 +3,18 @@ def ack(m, n):
         return "Invalid argument, must be absolute integers."
     if m == 0:
         return n + 1
-    elif m > 0 and n == 0:
-        return ack(m - 1, 1)
-    else:
-        return ack(m-1, ack(m, n-1))
-
+    elif m > 0:
+        if n == 0:
+            return ack(m-1, 1)
+        elif n > 0:
+            return ack(m-1, ack(m, n-1))
 
 if __name__ == "__main__":
     if ack(2, 2) == 7:
-        print "Test OK"
+        print "Test OK ack(2, 2)"
     if ack(3, 4) == 125:
-        print "Test OK"
+        print "Test OK ack(3, 4)"
+    if ack(-2, 3) is not int:
+        print "Test OK ack(-2, 3)"
+    if ack(0, 2) == 3:
+        print "Test OK ack(0, 2)"
