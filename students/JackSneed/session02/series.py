72f192f8b943e1e8286164ec452100b3a66b6f26
Jack <sneed.jack@gmail.com>
2015-03-22 10:45:55 -0700

    Add lucas function with assert test.

--- students/JackSneed/session02/series.py
+++ students/JackSneed/session02/series.py
@@ -7,6 +7,19 @@ def fabonacci(n):
 
     return val1
 
-#test fabonacci function works properly
+#Test if fabonacci function works properly
 if __name__ == '__main__':
     assert fabonacci(3) == 2
+
+def lucas(n):
+    """Return the nth value in the Lucas series."""
+
+    val1, val2 = 2, 1
+    for i in range(n):
+        val1, val2 = val2, val1 + val2
+
+    return val1
+
+#Test if lucas function works properly
+if __name__ == '__main__':
+    assert lucas(3) == 4
