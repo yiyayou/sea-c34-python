a3e4c69238fb2adf1fbc8e596c5777d098ee8e82
Jack <sneed.jack@gmail.com>
2015-03-22 11:31:12 -0700

    Add sum_series function and put assert statements at end of module.

--- students/JackSneed/session02/series.py
+++ students/JackSneed/session02/series.py
@@ -1,15 +1,11 @@
 def fabonacci(n):
-    """Return the nth value in the fibonacci series."""
+    """Return the nth value in the Fibonacci series."""
 
     val1, val2 = 0, 1
     for i in range(n):
         val1, val2 = val2, val1 + val2
 
     return val1
-
-#Test if fabonacci function works properly
-if __name__ == '__main__':
-    assert fabonacci(3) == 2
 
 def lucas(n):
     """Return the nth value in the Lucas series."""
@@ -20,6 +16,21 @@ def lucas(n):
 
     return val1
 
-#Test if lucas function works properly
+def sum_series(n):
+    """Return the nth value for sum_series.
+
+    Use optional parameters to produce other series.
+    """
+
+    val1, val2 = 3, 5
+    for i in range(n):
+        val1, val2 = val2, val1 + val2
+
+    return val1
+
+
+#Test if functions work properly
 if __name__ == '__main__':
+    assert fabonacci(3) == 2
     assert lucas(3) == 4
+    assert sum_series(3) == 13
