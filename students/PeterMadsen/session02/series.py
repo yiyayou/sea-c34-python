0038150573a93222ae90a25d55ce68beefe7abbd
Peter Madsen <cephas.ryle@gmail.com>
2015-03-22 02:01:45 -0700

    Added in Test Code and Fibonacci Documentation

--- students/PeterMadsen/session02/series.py
+++ students/PeterMadsen/session02/series.py
@@ -1,2 +1,24 @@
 def fibonacci(n):
-    return n + n
+    """
+    Calculates the nth value of the fibonacci series
+
+    Args: 
+        n: the number of the sequence desired
+    Returns:
+        nth value of the fibonacci series
+
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else: 
+        return fibonacci(n  - 1) + fibonacci(n - 2)
+
+
+
+# Test Code
+if __name__ == "__main__":
+    print("Testing fibonacci sequence")
+    for i in range (8):
+        print (str(fibonacci(i)) + ", "),
