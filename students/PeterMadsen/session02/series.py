e8513e31fcca59180ea04529478d640afbb8da63
Peter Madsen <cephas.ryle@gmail.com>
2015-03-22 02:27:09 -0700

    added in Series Function and Tests

--- students/PeterMadsen/session02/series.py
+++ students/PeterMadsen/session02/series.py
@@ -32,11 +32,42 @@ def lucas(n):
     else: 
         return lucas(n  - 1) + lucas(n - 2)
 
+def sum_series(n, first_value=0, second_value=1):
+    """
+    Calculates the nth value of the summation series
+
+    Args: 
+        n: the number of the series desired
+        first_value: The first value element for the given series defaulting to 0
+        second_value: The second element for the given series defaulting to 1
+    Returns:
+        nth value of the series startin with first_value and second_value
+
+    """
+    if n <= 0:
+        return first_value
+    elif n == 1:
+        return second_value
+    else: 
+        return sum_series(n  - 1) + sum_series(n - 2)
+
 # Test Code
 if __name__ == "__main__":
     print("Testing Fibonacci series")
     for i in range (8):
         print (str(fibonacci(i)) + ", "),
-    print("/nTesting Lucas series")
+    print("...")
+    print("Testing Lucas series")
     for i in range (8):
         print (str(lucas(i)) + ", "),
+    print("...")
+    # Testing Sum Series Function
+    print("Testing Fibonacci Series using generic sum_series option")
+    for i in range (8):
+        print (str(sum_series(i)) + ", "),
+    print("...")
+    print("Testing Lucas Series using sum_series function")
+    for i in range (8):
+        print (str(sum_series(i, 2, 1)) + ", "),
+    print("...")
+
