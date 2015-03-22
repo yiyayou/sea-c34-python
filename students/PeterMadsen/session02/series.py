8f459d73d77671c0b086c76f2fab0b0b48610edd
Peter Madsen <cephas.ryle@gmail.com>
2015-03-22 02:09:39 -0700

    Added in Lucas Function Case

--- students/PeterMadsen/session02/series.py
+++ students/PeterMadsen/session02/series.py
@@ -3,7 +3,7 @@ def fibonacci(n):
     Calculates the nth value of the fibonacci series
 
     Args: 
-        n: the number of the sequence desired
+        n: the number of the series desired
     Returns:
         nth value of the fibonacci series
 
@@ -15,10 +15,28 @@ def fibonacci(n):
     else: 
         return fibonacci(n  - 1) + fibonacci(n - 2)
 
+def lucas(n):
+    """
+    Calculates the nth value of the lucas series
 
+    Args: 
+        n: the number of the series desired
+    Returns:
+        nth value of the lucas series
+
+    """
+    if n <= 0:
+        return 2
+    elif n == 1:
+        return 1
+    else: 
+        return lucas(n  - 1) + lucas(n - 2)
 
 # Test Code
 if __name__ == "__main__":
-    print("Testing fibonacci sequence")
+    print("Testing Fibonacci series")
     for i in range (8):
         print (str(fibonacci(i)) + ", "),
+    print("/nTesting Lucas series")
+    for i in range (8):
+        print (str(lucas(i)) + ", "),
