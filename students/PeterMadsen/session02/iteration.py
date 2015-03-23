e45523327e2fe0899975351e6a0f956d22f6b7c5
Peter Madsen <cephas.ryle@gmail.com>
2015-03-22 21:28:26 -0700

    adding question 2 and improving test code in iteration.py

--- students/PeterMadsen/session02/iteration.py
+++ students/PeterMadsen/session02/iteration.py
@@ -1,15 +1,28 @@
-#Question 1: What does it look like when you iterate through a Boolean List?
+#Question 1: What does it look like when you iterate through a "Truthy" List?
 def question_one (test_list):
     """Prints "Happy" every time a given list is true"""
 
     for value in test_list:
-        print(value)
         if value:
-            print ("Happy")
+            print ("Happy"),
 
-#Question 2: What is the behaviour of "continue" in a for loop?
+#Question 2: What does "continue" vs "break" look like in  a range of 10?
 def question_two():
-    pass
+    """Prints the Values of two for loops inspecting the differences"""
+    
+    print ("Break Loop: ")
+    for i in range (10):
+        if i == 8:
+            break
+        else:
+            print(i), 
+
+    print("\nContinue Loop: ")
+    for i in range (10):
+        if i == 8:
+            continue
+        else: 
+            print(i), 
 
 #Question 3: 
 def question_three():
@@ -17,4 +30,7 @@ def question_three():
 
 # ************* TEST CODE *******************#
 if __name__ == '__main__':
+    print("Testing Question 1: What does it look like when you iterate through a 'Truthy' List?")
     question_one([True, False, True, 1, 0])
+    print('\nQuestion 2: What does "continue" vs "break" look like in  a range of 10?')
+    question_two()
