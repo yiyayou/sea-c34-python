1cc7cdc47a9889893b41e968c1935b8239178f87
Joseph O'Malley <joseph.t.omalley@gmail.com>
2015-03-23 14:07:18 -0700

    Add working lucas function.

--- students/JosephOmalley/session02/series.py
+++ students/JosephOmalley/session02/series.py
@@ -8,6 +8,15 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
+"""Lucas function"""
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
+
 
 
 
