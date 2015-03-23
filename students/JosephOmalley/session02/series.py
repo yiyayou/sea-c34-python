257f8b5ff16d53dbec06376d4cbc57dae0c2ed58
Joseph O'Malley <joseph.t.omalley@gmail.com>
2015-03-23 14:03:11 -0700

    Abandon list form and fix fibonacci function.

---
+++ students/JosephOmalley/session02/series.py
@@ -0,0 +1,17 @@
+"""Joseph O'Malley"""
+"""Fibonacci function"""
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+
+
+
+
+
+
