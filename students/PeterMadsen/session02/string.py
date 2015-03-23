c6dc7910d3c8491f1c30b7c1d3f432a95f246239
Peter Madsen <cephas.ryle@gmail.com>
2015-03-22 22:33:20 -0700

    adding in question Three and docstrings

--- students/PeterMadsen/session02/string.py
+++ students/PeterMadsen/session02/string.py
@@ -1,5 +1,6 @@
 #Question 1: How many string conversion types are there?
 def question_one():
+    """Prints the answer to the stated question but not the why?"""
     print ("There are %d different conversion types according to Python Documentation" % 16)
 
 #Question 2: Why do we have %i and %d for ints and %f and %F for floats?
@@ -10,8 +11,17 @@ def question_two():
     print ("Why are there %f ways to write a float" %2.0)
     print ("Why are there %F ways to write an float" %2.0)
 
+#Question 3: Will the .format option take over all string operations?
+def question_three():
+    """This function Asks an essential Question"""
+    print ('Will the {} method remain in use, or will'.format("classic")), 
+    print('.... the .%(method)s method take over the string formatting world?' % {u'method': u"format"})
+
 # **************** TEST CODE ***************************#
 if __name__ == '__main__':
+    print("Question 1: How many string conversion types are there?")
     question_one()
+    print ('#Question 2: Why do we have %i and %d for ints and %f and %F for floats?' )
     question_two()
-    
+    print ('#Question 3: Will the .format option take over all string operations?')
+    question_three()
