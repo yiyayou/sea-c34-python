0c105fd352cc7a31f55ffcaa316c5668ca2e8e32
Peter Madsen <cephas.ryle@gmail.com>
2015-03-22 22:01:41 -0700

    adding in Question One and TestCode

--- students/PeterMadsen/session02/string.py
+++ students/PeterMadsen/session02/string.py
@@ -1,1 +1,9 @@
-#Question 1
+#Question 1: How many string conversion types are there?
+def question_one():
+    print ("There are %d different conversion types according to Python Documentation" % 16)
+
+def question_two(): 
+
+# **************** TEST CODE ***************************#
+if __name__ == '__main__':
+    question_one()
