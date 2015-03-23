d966f701e1b026ce76e1298f1e9b082f767fe73b
Anna Clawson <seadeer@gmail.com>
2015-03-23 16:27:15 -0700

    moved files to session01 folder

---
+++ students/AnnaClawson/session01/break_me.py
@@ -0,0 +1,20 @@
+def break_me1():
+  if 5 + "Anna" > 6:
+    return true
+
+def break_me2()
+  if 5 + 1 == 6:
+    return true
+
+def break_me3():
+  anna = 100
+  return anna.age
+
+def break_me4():
+  if anna == 6:
+    return true
+
+break_me1()
+break_me2
+break_me3
+break_me4()
