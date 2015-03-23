0e273bf605d8937244df02d09b30b736032cf34c
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-03-22 23:22:26 -0700

    Add sum series function to series.py

--- students/JeffreyMendiola/session02/series.py
+++ students/JeffreyMendiola/session02/series.py
@@ -5,7 +5,7 @@ def fibonacci(n):
     """Return the nth value in the fibonacci series, starting at 0"""
     b1 = 0
     b2 = 1
-    if n > 2:
+    if n > 1:
         counter = 3
         while (counter <= n):
             b3 = b1 + b2
@@ -14,9 +14,9 @@ def fibonacci(n):
             counter += 1
         b3 = b1 + b2
     elif n > 0:
-        b3 = 1
+        b3 = b2
     else:
-        b3 = 0
+        b3 = b1
     return b3
 
 user_input = raw_input("Enter the index of fibonacci series: ")
@@ -40,11 +40,34 @@ def lucas(n):
             counter += 1
         b3 = b1 + b2
     elif n > 0:
-        b3 = 1
+        b3 = b2
     else:
-        b3 = 2
+        b3 = b1
     return b3
 
 user_input = raw_input("Enter the index of lucas series: ")
 user_input = int(user_input)
 print "The nth term is: " + str(lucas(user_input))
+
+
+# Sum Series
+
+def sum_series(x, y=0, z=1):
+    """Return the nth value in the sum series"""
+    b1 = y
+    b2 = z
+    if x > 1:
+        counter = 3
+        while (counter <= x):
+            b3 = b1 + b2
+            b1 = b2
+            b2 = b3
+            counter += 1
+        b3 = b1 + b2
+    elif x > 0:
+        b3 = b2
+    else:
+        b3 = b1
+    return b3
+
+print sum_series(0)
