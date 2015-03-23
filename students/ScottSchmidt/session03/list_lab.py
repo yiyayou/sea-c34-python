6a91ed8461c87e397a32411501c3d47d9021b2f0
Scott Schmidt <sjschmidt44@gmail.com>
2015-03-23 13:36:14 -0700

    Add working list_lab script to repo.

--- students/ScottSchmidt/session03/list_lab.py
+++ students/ScottSchmidt/session03/list_lab.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/env python
 
+# Section 1
 fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 print(fruit)
 
@@ -20,6 +21,7 @@ for item in fruit:
     if item[0] is "P":
         print item
 
+# Section 2
 print(fruit)
 
 del fruit[-1]
@@ -32,3 +34,41 @@ else:
     while user3 in fruit:
         fruit.remove(user3)
     print(fruit)
+
+# Section 3
+while len(fruit) > 0:
+    for item in fruit:
+        user4 = raw_input("Do you like " + item.lower() + "? ")
+        while user4 != "Yes" and user4 != "No":
+            user4 = raw_input("Please enter Yes or No: ")
+        else:
+            if user4 == "No" or user4 == "no":
+                while item in fruit:
+                    fruit.remove(item)
+                    print(fruit)
+            else:
+                user4 = raw_input("Do you want to continue? ")
+                while user4 != "Yes" and user4 != "No":
+                    user4 = raw_input("Please enter Yes or No")
+                else:
+                    if user4 is "No":
+                        break
+                print(fruit)
+if len(fruit) < 1:
+    print("The list is empty!")
+else:
+    print (fruit)
+
+# Section 4
+fruit2 = fruit
+print(fruit2)
+fruit3 = []
+
+for word in fruit2:
+    revWord = word[::-1]
+    fruit3.append(revWord)
+print(fruit3)
+
+del fruit[-1]
+print(fruit)
+print(fruit3)
