e246a46f28f127367a4704e332631c146956e68d
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-03-22 22:25:52 -0700

    Add lucas number function to series.py

--- students/JeffreyMendiola/session02/series.py
+++ students/JeffreyMendiola/session02/series.py
@@ -22,3 +22,29 @@ def fibonacci(n):
 user_input = raw_input("Enter the index of fibonacci series: ")
 user_input = int(user_input)
 print "The nth term is: " + str(fibonacci(user_input))
+
+
+# Lucas Numbers
+# 2, 1, 3, 4, 7, 11, 18, 29, ...
+
+def lucas(n):
+    """Return the nth value in the lucas series"""
+    b1 = 2
+    b2 = 1
+    if n > 1:
+        counter = 3
+        while (counter <= n):
+            b3 = b1 + b2
+            b1 = b2
+            b2 = b3
+            counter += 1
+        b3 = b1 + b2
+    elif n > 0:
+        b3 = 1
+    else:
+        b3 = 2
+    return b3
+
+user_input = raw_input("Enter the index of lucas series: ")
+user_input = int(user_input)
+print "The nth term is: " + str(lucas(user_input))
