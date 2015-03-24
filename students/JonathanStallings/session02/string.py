d0e7759d33a9eafbb7a813b974674030d44a9c03
Jonathan Stallings <jonathanstallings@outlook.com>
2015-03-24 16:00:20 -0700

    Update string.py docstrings with questions for assignment.

--- students/JonathanStallings/session02/string.py
+++ students/JonathanStallings/session02/string.py
@@ -1,48 +1,31 @@
-def highest_ordinal(string):
-    """
-    Return the highest ordinal in a given string.
-
-    Args:
-        string: the string to be checked.
-    Returns:
-        the highest ordinal value in the string.
-    """
-    msg = u"The max was {maxOrd} and value was {maxValue}." \
+def highest_ordinal():
+    """Which letter has the highest ordinal value?"""
+    string = u"The quick brown fox leaped!"
+    msg = u"The max was {maxOrd} and its value was {maxValue}." \
         .format(maxOrd=max(string), maxValue=ord(max(string)))
     print(msg)
 
+# highest_ordinal()
+# result: The max was x and its value was 120.
 
-# highest_ordinal(u"The quick brown fox leaped!")
-# result: The max was x and the value was 120.
 
-def lowercase_number(num):
-    """
-    Attempt to lowercase a number to see what happens.
-
-    Args:
-        num: the number to be lowercased
-    Returns:
-        the result.
-    """
+def lowercase_number():
+    """What happens when you try to lowercase a number?"""
+    num = 16
     try:
         print num.lower()
     except:
         print u"Use of lowercase method on number raises exception."
 
-# lowercase_number(16)
+# lowercase_number()
 # result: Using a lowercase (string) method on number throws an error.
 
 
 def input_type():
-    """
-    Check type of user input.
-
-    Returns:
-        the type of user input.
-    """
+    """Does raw_input always return a string?"""
     user_input = raw_input()
     print type(user_input)
 
 
 # input_type()
-# result: raw_input always returns a string.
+# result: Yes. raw_input always returns a string.
