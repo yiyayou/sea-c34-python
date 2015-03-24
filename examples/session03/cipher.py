d4d98e743b451605d78ceea78dfb0d079ef6da6b
Yi Gu <yigu404@gmail.com>
2015-03-23 20:36:28 -0700

    change for session03 first part of class

--- examples/session03/cipher.py
+++ examples/session03/cipher.py
@@ -1,16 +1,15 @@
 import random
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyz "
 
 alphabet_list = [alphabet[i] for i in range(len(alphabet))]
 #alphabet_list = list(alphabet)
 
 print(alphabet_list)
 
-message_text = "what is a baggins"
+message_text = "hello"
 
-cipher_list = list(alphabet_list)
-random.shuffle(cipher_list)
+cipher_list =['v', 'y', 'h', 'l', 'a', 'n', 'i', 'g', 'e', 'd', 'k', 'c', 'w', ' ', 'f', 's', 'm', 'p', 'r', 't', 'o', 'u', 'z', 'b', 'j', 'x', 'q']
 
 cipher_text = ""
 
