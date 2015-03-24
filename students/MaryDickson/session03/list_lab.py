a408802f4b41e2138b0dbf4f5306c016248ab7e9
Mary Dickson <mdickson@gmail.com>
2015-03-23 20:56:02 -0700

    add list_lab.py

---
+++ students/MaryDickson/session03/list_lab.py
@@ -0,0 +1,34 @@
+"""
+When the script is run, it should accomplish the following four series of actions:
+
+Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+Display the list.
+Ask the user for another fruit and add it to the end of the list.
+Display the list.
+Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis).
+Add another fruit to the beginning of the list using “+” and display the list.
+Add another fruit to the beginning of the list using insert() and display the list.
+Display all the fruits that begin with “P”, using a for loop.
+Using the list created in series 1 above:
+
+Display the list.
+Remove the last fruit from the list.
+Display the list.
+Ask the user for a fruit to delete and find it and delete it.
+(Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
+Again, using the list from series 1:
+
+Ask the user for input displaying a line like “Do you like apples?”
+for each fruit in the list (making the fruit all lowercase).
+For each “no”, delete that fruit from the list.
+For any answer that is not “yes” or “no”, prompt the user to answer with one of those two values (a while loop is good here):
+Display the list.
+Once more, using the list from series 1:
+
+Make a copy of the list and reverse the letters in each fruit in the copy.
+Delete the last item of the original list. Display the original list and the copy.
+Commit and push to the task6 branch, then submit a pull request to the main class repo.
+
+Finally, submit your assignment in Canvas by giving the URL of the pull request.
+
+"""
