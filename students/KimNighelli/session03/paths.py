ad05d711db61dbb77187e52a4479e3875f819cd0
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-23 19:09:59 -0700

    Made paths.py have a function

--- students/KimNighelli/session03/paths.py
+++ students/KimNighelli/session03/paths.py
@@ -5,36 +5,40 @@ This is for Task 8 - Paths
 
 I had a few questions about paths.
 
-1) What is my current absolute path for this file? What's the difference between
-basename and dirname? Can OS print out all the files in the current directory?
+1) Can I print out every file in my directory using the OS module and defining some path?
 '''
 
-print "The absolute path to this file is %s" % (os.path.abspath('paths.py'))
+def path_info(path):
 
-print "The directory that this program is housed in is %s" % (os.path.dirname('~/Desktop/CodeFellows/sea-c34-python/students/KimNighelli/session03/paths.py'))
+    absolute_path = os.path.abspath(path)
+    basename = os.path.basename(path)
 
-print "The basename of this program is %s" % (os.path.basename('./paths.py'))
+    print "The absolute path to this file is %s" % (absolute_path)
 
-# Print all the files in a set directory
+    print "The basename of this program is %s" % (basename)
 
-print "All the current files in this directory are: "
-path = '.' #Current working directory
-for file in os.listdir(path):
-    print file
+    # Print all the files in a set directory
+
+    print "\nAll the current files in this directory are: "
+    path = '.' #Current working directory
+    for file in os.listdir(path):
+        print file
+
+    return absolute_path
+
+if __name__ == "__main__":
+    
+    path_info('./paths.py')
 
 '''
-It appears that dirname and basename come from the same path, with basename being the last part of the path,
-and dirname being the full path of the directory that the object you specify is housed in. 
-
 Output from the print statements:
 
 The absolute path to this file is /Users/knighelli/Desktop/CodeFellows/sea-c34-python/students/KimNighelli/session03/paths.py
-The directory that this program is housed in is ~/Desktop/CodeFellows/sea-c34-python/students/KimNighelli/session03
 The basename of this program is paths.py
+
 All the current files in this directory are:
     dictionaries.py
     exceptions.py
     files.py
     paths.py
-
 '''
