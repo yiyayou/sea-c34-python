668d5ed281dd8aae0faa1206fd8daa4a031b4424
davidmshou <david.m.shou@gmail.com>
2015-03-24 09:35:05 -0700

    First series.

--- students/DavidShoubridge/session03/list_lab.py
+++ students/DavidShoubridge/session03/list_lab.py
@@ -1,2 +1,35 @@
 #!/usr/bin/env python
 
+my_list = ["Apples", "Pears", "Oranges", "Peaches"]
+
+print(my_list)
+
+
+def user_input():
+    fruit_input = raw_input("Give me a fruit to add to our list:")
+
+    my_list.append(fruit_input)
+
+    print(my_list)
+
+    number_input = raw_input("Enter a number:")
+
+    print(number_input)
+
+    number_input = int(number_input)
+
+    print(my_list[(number_input - 1)])
+
+user_input()
+
+# my_list[0] = "Bananas"
+
+# print my_list
+
+my_list.insert(0, "Pineapple")
+
+print(my_list)
+
+for fruit in my_list:
+    if fruit[0] == "P":
+        print fruit
