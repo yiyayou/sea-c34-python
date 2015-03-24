4ff9642b42ebc5802657647aa1b57d0b3fbe8041
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-23 20:15:03 -0700

    Reversed the cipher

--- examples/session03/cipher.py
+++ examples/session03/cipher.py
@@ -1,22 +1,25 @@
 import random
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyz "
 
 alphabet_list = [alphabet[i] for i in range(len(alphabet))]
 #alphabet_list = list(alphabet)
 
 print(alphabet_list)
 
-message_text = "what is a baggins"
+message_text = "gaccfqtarte iqtarte i"
 
-cipher_list = list(alphabet_list)
-random.shuffle(cipher_list)
+#cipher_list = list(alphabet_list)
+#rdm = random.shuffle(cipher_list)
+
+cipher_list= ['v', 'y', 'h', 'l', 'a', 'n', 'i', 'g', 'e', 'd', 'k', 'c', 'w', ' ', 'f', 's', 'm', 'p', 'r', 't', 'o', 'u', 'z', 'b', 'j', 'x', 'q']
 
 cipher_text = ""
 
 for x in message_text:
-    index = alphabet_list.index(x)
-    cipher_char = cipher_list[index]
+    index = cipher_list.index(x)
+    cipher_char = alphabet_list[index]
     cipher_text += cipher_char
+
 
 print cipher_text
