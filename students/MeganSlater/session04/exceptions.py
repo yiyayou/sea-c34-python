6175d388a947dc57d48f579688f6b70bd22042bd
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 16:57:51 -0700

    add two questions

---
+++ students/MeganSlater/session04/exceptions.py
@@ -0,0 +1,27 @@
+"""Can I write my own exception?"""
+# define new class for my exception
+
+
+class BadIdea(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+try:
+    raise BadIdea(2*2)
+except BadIdea as e:
+    print 'My exception occurred, value:', e.value
+# raise exception on purpose
+raise BadIdea('Yes you can!')
+
+
+"""Will the script still run after the exception has thrown?"""
+for number in range(6):
+    fraction = "undefined"
+    try:                      # first action
+        fraction = 6.0/number
+    except ZeroDivisionError:  # runs if first action creates error
+        print("you cannot divide by 0!")
+    finally:
+        print fraction  # runs no matter what
