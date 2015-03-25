f39f03f38d9abc75ddaf04eee3e29caad0abb2d2
davidmshou <david.m.shou@gmail.com>
2015-03-25 11:47:36 -0700

    Add file creation with path.

---
+++ students/DavidShoubridge/session03/paths.py
@@ -0,0 +1,6 @@
+def create_with_path():
+    """ Can I create a new file using an absolute path?
+    """
+    open("/Users/David/foundations-2-python/sea-c34-python/students/DavidShoubridge/session03/new.txt", "wb")
+
+create_with_path()
