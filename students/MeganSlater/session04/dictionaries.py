a965826da17fd194e313800d762e8336931a2e6c
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 16:56:12 -0700

    debugged

--- students/MeganSlater/session04/dictionaries.py
+++ students/MeganSlater/session04/dictionaries.py
@@ -13,18 +13,19 @@ unfreeze = set("")
 # add items from frozen set to set
 for item in frozen:
     unfreeze.add(item)
+unfreeze.add(5)
 print unfreeze
 
 
 """What do all those set operations do?"""
 newset = set("Cheese is Awesome!")
 other = set("Cheese")
-newset.isdisjoint(other)  # check if two sets have no elements in common
-newset.issubset(other)  # check if one set is subset of the other
-newset.union(other)  # puts the sets together
-newset.intersection(other)  # returns all values the sets have in common
-newset.difference(other)  # returns all values the sets do not have in common
-newset.symmetric_difference(other)  # returns values that exist in only one set
+print newset.isdisjoint(other)  # check if two sets have no elements in common
+print newset.issubset(other)  # check if one set is subset of the other
+print newset.union(other)  # puts the sets together
+print newset.intersection(other)  # returns all values the sets have in common
+print newset.difference(other)  # returns values the sets do not have in common
+print newset.symmetric_difference(other)  # returns values only one set
 
 """Can I change a set into a list?"""
 newset = set(["Apple", "Pear", "Orange", "Banana"])
@@ -33,3 +34,4 @@ newlist = []
 # add all items in set to new list
 for fruit in newset:
     newlist.append(fruit)
+print newlist
