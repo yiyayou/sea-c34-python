748fec04793888035deb6fcd5175e71dbea654a6
Jack <sneed.jack@gmail.com>
2015-03-25 14:05:42 -0700

    Add unicode to List_lab.py

--- students/JackSneed/session03/list_lab.py
+++ students/JackSneed/session03/list_lab.py
@@ -5,25 +5,25 @@ def fruit_list():
 
     #List Lab part 1
 
-    fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+    fruit = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
     print(fruit)
 
-    new_fruit = raw_input("Name a new fruit: ")
+    new_fruit = raw_input(u"Name a new fruit: ")
     fruit.append(new_fruit)
     print(fruit)
 
-    user_pick = int(raw_input("Pick a number 1-5: "))
+    user_pick = int(raw_input(u"Pick a number 1-5: "))
     fruit_chosen = fruit[user_pick - 1]
-    print("The number %i fruit on the index is %s." % (user_pick, fruit_chosen))
+    print(u"The number %i fruit on the index is %s." % (user_pick, fruit_chosen))
 
-    fruit = ["Plums"] + fruit
+    fruit = [u"Plums"] + fruit
     print(fruit)
-    fruit.insert(0, "Persimmons")
+    fruit.insert(0, u"Persimmons")
     print(fruit)
 
     for item in fruit:
-        if item.startswith("P"):
-            print("This fruit starts with P: %s" % (item))
+        if item.startswith(u"P"):
+            print(u"This fruit starts with P: %s" % (item))
 
     #List Lab part 2
 
@@ -31,18 +31,18 @@ def fruit_list():
     del fruit[-1]
     print(fruit)
 
-    user_pick2 = (raw_input("Choose a fruit from the index to remove: "))
+    user_pick2 = (raw_input(u"Choose a fruit from the index to remove: "))
     fruit.remove(user_pick2)
     print(fruit)
 
     #List Lab part 3
 
     for item in fruit[:]:
-        answer = ""
-        while answer != "yes" and answer != "no":
-            answer = raw_input("Do you like %s? (Enter yes or no): " % (item.lower()))
+        answer = u""
+        while answer != u"yes" and answer != u"no":
+            answer = raw_input(u"Do you like %s? (Enter yes or no): " % (item.lower()))
 
-        if answer == "no":
+        if answer == u"no":
             fruit.remove(item)
 
     print(fruit)
