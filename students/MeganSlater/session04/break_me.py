998577734b9f2efc77df69d49e44fa3c6f6546be
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 16:47:47 -0700

    moving to proper location

---
+++ students/MeganSlater/session04/break_me.py
@@ -0,0 +1,13 @@
+#here's my NameError  
+#def fun():
+#   print (a+b)
+#here's my TypeError 
+#def fun():
+#   print ('fish'+ 20)
+#here's my SyntaxError 
+#def fun():
+#   printI Like Coffee
+#here's my AttributeError
+def fun():
+    a = 5
+    return a.color
