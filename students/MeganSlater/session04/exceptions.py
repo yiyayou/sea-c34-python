5a9a6c9a621a2071ca968be8611fc77c684f757d
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 20:19:23 -0700

    made everything into a function

--- students/MeganSlater/session04/exceptions.py
+++ students/MeganSlater/session04/exceptions.py
@@ -3,25 +3,30 @@ Can I write my own exception?"""
 # define new class for my exception
 
 
-class BadIdea(Exception):
-    def __init__(self, value):
-        self.value = value
+def make_exception():
+    class BadIdea(Exception):
+        def __init__(self, value):
+            self.value = value
 
-    def __str__(self):
-        return repr(self.value)
+        def __str__(self):
+            return repr(self.value)
 
-try:
-    raise BadIdea(2*2)
-except BadIdea as e:
-    print 'My exception occurred, value:', e.value
-
+    try:
+        raise BadIdea(2*2)
+    except BadIdea as e:
+        print 'My exception occurred, value:', e.value
+make_exception()
 
 """Will the script still run after the exception has thrown?"""
-for number in range(6):
-    fraction = "undefined"
-    try:                      # first action
-        fraction = 6.0/number
-    except ZeroDivisionError:  # runs if first action creates error
-        print("you cannot divide by 0!")
-    finally:
-        print fraction  # runs no matter what
+
+
+def still_running():
+    for number in range(6):
+        fraction = "undefined"
+        try:                      # first action
+            fraction = 6.0/number
+        except ZeroDivisionError:  # runs if first action creates error
+            print("you cannot divide by 0!")
+        finally:
+            print fraction  # runs no matter what
+still_running()
