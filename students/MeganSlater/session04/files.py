d1b38d4f444c5d7a23aed1c313f351f4f05f0cce
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 17:06:30 -0700

    debugged

--- students/MeganSlater/session04/files.py
+++ students/MeganSlater/session04/files.py
@@ -1,10 +1,10 @@
 """Do I have to close every file I open?"""
 import io
-f = io.open('secrets.txt', encoding-'utf-8')
+f = io.open('secrets.txt', encoding='utf-8')
 secret_data = f.read
 print "Is this thing still running?"
 
 
 """Can I edit the file once I open it?  How?"""
-f = io.open('secrets.txt', r+)
-f.write("I'm editing this file")
+f = io.open('secrets.txt', 'r+')
+f.write(u"I'm editing this file")
