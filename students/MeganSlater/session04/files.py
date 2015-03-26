d9dfff7ab44cad1e4ff914506647d811a4358588
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 20:20:24 -0700

    made everything into a function

--- students/MeganSlater/session04/files.py
+++ students/MeganSlater/session04/files.py
@@ -1,10 +1,16 @@
 """Do I have to close every file I open?"""
-import io
-f = io.open('secrets.txt', encoding='utf-8')
-secret_data = f.read
-print "Is this thing still running?"
+
+
+def open_file():
+    import io
+    f = io.open('secrets.txt', encoding='utf-8')
+    secret_data = f.read
+    print "Is this thing still running?"
 
 
 """Can I edit the file once I open it?  How?"""
-f = io.open('secrets.txt', 'r+')
-f.write(u"I'm editing this file")
+
+
+def edit_file():
+    f = io.open('secrets.txt', 'r+')
+    f.write(u"I'm editing this file")
