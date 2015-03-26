baeaeeeb0e3f2ce283e8322546a43c1b8b1df0fd
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 17:11:34 -0700

    debugged

--- students/MeganSlater/session04/paths.py
+++ students/MeganSlater/session04/paths.py
@@ -1,6 +1,7 @@
 """Can paths be used for anything other than opening a file?"""
 import os
 # we can print the current working directory
-print "The current working directory is", os.getcwd()
+cwd = os.getcwd()
+print "The current working directory is " + cwd
 # we can see all the files in that directory
-print os.listdir("newdir")
+print os.listdir(cwd)
