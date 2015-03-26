ca6d704d178c05f6d8e035fce85d4702dbad481c
Megan Slater <megan.e.slater@gmail.com>
2015-03-25 17:02:03 -0700

    debugged

--- students/MeganSlater/session04/exceptions.py
+++ students/MeganSlater/session04/exceptions.py
@@ -1,10 +1,12 @@
-"""Can I write my own exception?"""
+"""
+Can I write my own exception?"""
 # define new class for my exception
 
 
 class BadIdea(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
@@ -12,8 +14,6 @@ try:
     raise BadIdea(2*2)
 except BadIdea as e:
     print 'My exception occurred, value:', e.value
-# raise exception on purpose
-raise BadIdea('Yes you can!')
 
 
 """Will the script still run after the exception has thrown?"""
