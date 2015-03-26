da5e6bd4f0d8e79fe695dc1ddd4969df802eed72
Megan Slater <megan.e.slater@gmail.com>
2015-03-26 16:43:49 -0700

    part 2 done

--- students/MeganSlater/session04/dict_lab.py
+++ students/MeganSlater/session04/dict_lab.py
@@ -1,3 +1,4 @@
+# part 1
 dict = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
 print dict
 del dict["cake"]
@@ -11,7 +12,12 @@ for key, value in dict.iteritems():
     if value == "Mango":
         print True
 
-
+# part 2
+numberdict = {}
 nums = []
 for i in range(1, 16):
         nums.append(i)
+hex = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1A", "1B", "1C", "1D", "1E"]
+for i, j in zip(nums, hex):
+    numberdict.update({i: j})
+print numberdict
