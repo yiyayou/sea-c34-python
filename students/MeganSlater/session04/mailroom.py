f3100425f99349d05632c8f3b04d6d506d3c8fa1
Megan Slater <megan.e.slater@gmail.com>
2015-03-26 21:44:35 -0700

    debugged send a thank you

--- students/MeganSlater/session04/mailroom.py
+++ students/MeganSlater/session04/mailroom.py
@@ -1,12 +1,11 @@
-donors = {"Donor Full Name": [amount donated, second amount donated]}
+donors = {"Megan Slater": []}
 
 
 def safe_input(x):
     try:
-        raw_input(x)
+        return raw_input(x)
     except (EOFError, KeyboardInterrupt):
-        return "None"
-safe_input("What is your favorite color? ")
+        pass
 
 
 # define a function that will either send a thank you or create a report
@@ -14,49 +13,43 @@ def thankyous():
     """ask user whether to send thank you or create report.  Take action based
     on input
     """
-    userprompt = ask_for_input("Choose Send a Thank You or Create a Report. ")
-    userprompt = userprompt.lower_case()
+    userprompt = safe_input("Choose Send a Thank You or Create a Report. ")
+    userprompt = userprompt.lower()
 
-    if userprompt == "Send a Thank You":
-        donorname = empty_string
-        while donorname does not equal "list":
-            donorname = ask_for_input("What is the donor's full name?")
+    if userprompt == "send a thank you":
+        donorname = "list"
+        while donorname == "list":
+            donorname = safe_input("What is the donor's full name? ")
             """if the user doesn't know any names the user will input 'list' and we
             print all the names and let them choose one
             """
             if donorname == "list":
-                for name, donation in donors:
+                for name, donation in donors.iteritems():
                     print name
-            """if instead of sending a thank you the user wants to start over they can
-            type quit and go back to the beginning
-            """
-            elif donorname == "quit":
-                thankyous()
         """If the user enters an unknown name we add that name to the dictionary
             and return to the beginning.
         """
-        for name, donation in donors:
-            if donorname does not equal any dictionary "name":
-                donors["name"] = 0
+        for name, donation in donors.iteritems():
+            if donorname != name:
+                donors[donorname] = []
                 thankyous()
                 """If the donor name is in the dictionary we prompt the user for
                 a donated amount and enter it into the dictionary"""
             else:
                 amount = ""
 # keep asking the user about how much was donated until they enter a number
-                while amount_is_not_a_number:
-                    amount = ask_for_input("How much did they donate? ")
-                    # if the user wants out here they can go to the beginning
-                    if amount == "quit":
-                        thankyous()
-                donors["name"] = amount
+                while amount.isdigit() == False:
+                    amount = safe_input("How much did they donate? ")
+                    donors[name] = donation + [amount]
+                break
+
         """Once an amount has been added the user will receive a print
         out of an email for every donor and return to the beginning.
         """
-        for name, donation in donors:
-            print("""Thank you " + donor + " for your generous donation
-                 of $" + donation ".  We couldn't do it without you!""")
+        for name, donation in donors.iteritems():
+            print("Thank you " + name + ".")
         thankyous()
+"""
 # If the user wants a report at the beginning we give them one
     elif userprompt == "Create a Report":
         totaldonated = []
@@ -85,5 +78,5 @@ def thankyous():
     else:
         print("I'm sorry, I don't understand.  Try again. ")
         thankyous()
-
+"""
 thankyous()
