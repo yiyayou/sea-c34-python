ff91f59a700740997366caa278da091068a13de9
Megan Slater <megan.e.slater@gmail.com>
2015-03-26 17:20:16 -0700

    add comments

--- students/MeganSlater/session04/dict_lab.py
+++ students/MeganSlater/session04/dict_lab.py
@@ -1,14 +1,14 @@
 # part 1
 dict = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
 print dict
-del dict["cake"]
+del dict["cake"]                        # remove cake from dictionary
 print dict
-dict.update({"fruit": "Mango"})
+dict.update({"fruit": "Mango"})  # add mango to dictionary
 print dict
-print dict.keys()
-print dict.values()
-print 'cake' in dict
-for key, value in dict.iteritems():
+print dict.keys()                           # print all dictionary keys
+print dict.values()                        # print all dictionary values
+print 'cake' in dict                        # true if cake is key in dictionary
+for key, value in dict.iteritems():  # true if Mango is value in dictionary
     if value == "Mango":
         print True
 
@@ -18,11 +18,13 @@ nums = []
 for i in range(1, 16):
         nums.append(i)
 hex = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1A", "1B", "1C", "1D", "1E"]
+# combine lists nums and hex to create key value pairs in numberdict
 for i, j in zip(nums, hex):
     numberdict.update({i: j})
 print numberdict
 
 # part 3
+# makes new dict with the numbers of 'a' in value as the new value
 a_dict = {}
 for key, value in dict.iteritems():
     a_dict.update({key: value.count('a')})
@@ -41,5 +43,7 @@ print s4.issubset(s2)
 python = set(['P', 'y', 't', 'h', 'o', 'n'])
 python.add('i')
 marathon = frozenset(['m', 'a', 'r', 'a', 't', 'h', 'o', 'n'])
+# prints all letters python and marathon do not have in common
 print python.union(marathon)
+# prints all letters python and marathon do have in common
 print python.intersection(marathon)
