7ea21b76fd9c435ca2a0eecbaff0629352f23a89
Jack <sneed.jack@gmail.com>
2015-03-27 16:03:29 -0700

    Fix series01 and add series02

--- students/JackSneed/session04/dict_lab.py
+++ students/JackSneed/session04/dict_lab.py
@@ -8,7 +8,23 @@ def series01():
     lab["fruit"] = "Mango"
     print(lab.keys())
     print(lab.values())
-    print("cake" in lab)
-    print(lab["fruit"])
+    key = lab.keys()
+    print("cake" in key)
+    value = lab.values()
+    print("Mango" in value)
 
 series01()
+
+
+
+def series02():
+    """Using the dict constructor and zip, build a dictionary of numbers
+    from zero to fifteen and the hexadecimal equivalent.
+    """
+
+    numbers = range(0, 16)
+    for hexadecimal in numbers:
+        result = dict(zip(numbers, [hex(hexadecimal)]))
+        print(result)
+
+series02()
