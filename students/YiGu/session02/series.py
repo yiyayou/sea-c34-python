57815bffeee363b4c3dafe69c7c70b69c7b5fff2
Yi Gu <yigu404@gmail.com>
2015-03-28 14:38:10 -0700

    Updated series.py.1.updated comment;2.updated sum_series;3.made correction to format

--- students/YiGu/session02/series.py
+++ students/YiGu/session02/series.py
@@ -1,48 +1,69 @@
-#series.py module
+# series.py module
+
 
 def fibonacci(n):
-	"""The Fibonacci Series is a numeric series starting with the integers 0 and 1.
-	In this series, the next integer is determined by summing the previous two.
-	This gives us:0, 1, 1, 2, 3, 5, 8, 13, ..."""
-	if n <=0:
-		return 0
-	elif n==1:
-		return 1
-	else:
-		return fibonacci(n-1)+fibonacci(n-2)
+    """This is Fibonacci fuction. The function return the nth value in the
+     fibonacci series"""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
 
 def lucas(n):
-	"""The Lucas Numbers are a related series of integers that start with the values
-	2 and 1 rather than 0 and 1. The resulting series looks like this:2, 1, 3, 4, 7, 11, 18, 29, ..."""
-	if n <=0:
-		return 2
-	elif n==1:
-		return 1
-	else:
-		return lucas(n-1)+lucas(n-2)
+    """This is lucas fuction. The function return the nth value in the lucas
+     series"""
+    if n <= 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-1)+lucas(n-2)
 
-def sum_series(x,y=0,z=1):
-	"""sum_series has three parameteres x,y,z. x is the required parameter
-	will determine which element in the series to print.
-	y and z are the optional parameters will have default values of 0 and 1
-	and will determine the first two values for the series to be produced.
-	Calling this function with no optional parameters will produce numbers from the fibonacci series.
-	Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers.
-	Other values for the optional parameters will produce other series."""
-	if x<0:
-		print "series should not be less than 0"
-		return None
-	elif y==2 and z==1:
-		return lucas(x)
-	else:
-		return fibonacci(x)
 
-#Main
+def sum_series(x, y=0, z=1):
+		"""sum_series has three parameteres x,y,z. x is the required parameter
+will determine which element in the series to print.
+y and z are the optional parameters will have default values of 0 and 1
+and will determine the first two values for the series to be produced.
+Calling this function with no optional parameters will produce numbers
+from the fibonacci series. Calling it with the optional arguments 2 and
+1 will produce values from the lucas numbers. Other values for the
+optional parameters will produce other series."""
+		if x < 0:
+				print "series should not be less than 0"
+		    return None
+		elif y == 2 and z == 1:
+		    return lucas(x)
+		else:
+		    return fibonacci_for_sum_sum_series(x, y, z)
+
+
+def fibonacci_for_sum_sum_series(x, y, z):
+	"""This is Fibonacci_for_sum_sum_series fuction. The function return the nth
+value in the fibonacci series but user can change 0th & 1th fibonacci series
+value"""
+	if x == 0:
+	    return y
+	elif x == 1:
+	    return z
+	else:
+	    return fibonacci(n-1)+fibonacci(n-2)
+
+# Main
 if __name__ == "__main__":
-	#assert statment
-	assert sum_series(0)==0 #fibonacci
-	assert sum_series(1)==1
-	assert sum_series(5)==5
-	assert sum_series(6)==8
-	assert sum_series(0,2,1)==2 #lucas
-	assert sum_series(5,2,1)==11
+    # assert statment
+    # fibonacci
+    assert fibonacci(0) == 0
+    assert fibonacci(5) == 5
+    assert sum_series(0) == 0
+    # fibonacci
+    assert sum_series(1) == 1
+    assert sum_series(5) == 5
+    assert sum_series(6) == 8
+    assert sum_series(0, 2, 1) == 2
+    # lucas
+    assert sum_series(5, 2, 1) == 11
+    sum_series(5, 3, 4)
