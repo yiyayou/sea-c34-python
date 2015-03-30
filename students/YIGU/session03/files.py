f591b5c817a5db328fe73b0aa867ee162be42ea8
Yi Gu <yigu404@gmail.com>
2015-03-29 20:21:11 -0700

    Added files.py

---
+++ students/YIGU/session03/files.py
@@ -0,0 +1,18 @@
+# 1. What happen when I try to read (500) with an empty file.
+
+header_size = 5000
+f = open('secrets.txt')
+secret_header = f.read(header_size)
+secret_rest = f.read()
+f.close()
+
+# Answer: nothing. No error.
+
+# 2. Can I read hidden file?
+import io
+f = io.open('.secrets.txt', encoding='utf-8')
+secret_data = f.read()
+print secret_data
+f.close()
+
+# Answer:Yes
