3c1d647a79c069ab2c418ff10c88252be4c41eac
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-30 13:40:18 -0700

    began functional.py

---
+++ students/KimNighelli/session04/functional.py
@@ -0,0 +1,11 @@
+'''
+This is for Task 12- Exploring Session 5- Lambdas and
+Functional Programming
+
+The following functions were written to help clarify some
+questions and misunderstandings I had while reviewing the
+session 5 lecture notes. Each question will be noted by a
+comment line #
+
+Kimberlee Nighelli - 30 March 2015
+'''
