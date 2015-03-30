d72b60eac01fd0af57af6c57ffcfa27d742780f4
Kimberlee Nighelli <knighelli@gmail.com>
2015-03-30 14:15:55 -0700

    Wrote question 2 for functional

--- students/KimNighelli/session04/functional.py
+++ students/KimNighelli/session04/functional.py
@@ -21,7 +21,18 @@ def maps_and_lambdas(a_list):
     divisible by 3, it returns value * 5. If it is divisible
     by 3, it returns the original value
     '''
-    return map(lambda x: x * 5 if (x % 3) else x, a_list)
+    return map(lambda x: x * 5 if x % 3 else x, a_list)
+
+
+# Question 2: Given a list, can I use a lambda function to
+# remove the values that are not alphanumberic?
+
+def alpha_lambda(a_list):
+    '''
+    Given a list, this returns only the alphanumeric
+    values in the list
+    '''
+    return filter(lambda x: x.isalnum(), a_list)
 
 
 
@@ -29,3 +40,6 @@ if __name__ == "__main__":
 
     TEST_LIST = [2, 3, 5, 6, 7, 9, 10, 12]
     print maps_and_lambdas(TEST_LIST)
+
+    TEST_LIST_2 = ["Book", "67@@kdhgfkj", "1234", "&&3ihappy!", "801dexter"]
+    print alpha_lambda(TEST_LIST_2)
