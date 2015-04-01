6ce14e9dbd351dbefa7dcdcb7450e10d0ee83bcd
yiyayou <yigu404@gmail.com>
2015-03-31 22:10:51 -0700

    Update safe_input.py

--- students/YIGU/session04/safe_input.py
+++ students/YIGU/session04/safe_input.py
@@ -5,6 +5,7 @@ def safe_input(note):
     """This function catch excepition from raw_input function and return None"""
     try:
         i = raw_input(note)
-        return i
     except (EOFError, KeyboardInterrupt):
         return None
+    else:
+        return i
