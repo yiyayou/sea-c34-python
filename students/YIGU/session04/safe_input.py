a0c96dc80fa85f1d5af556772662eb8c81e875df
Yi Gu <yigu404@gmail.com>
2015-03-31 21:49:51 -0700

    Remove safe_input.py

--- students/YIGU/session04/safe_input.py
+++
@@ -1,9 +0,0 @@
-# taks10 safe_input.py
-
-
-def safe_input(i):
-    """This function catch excepition from raw_input function and return None"""
-    try:
-        raw_input(i)
-    except (EOFError, KeyboardInterrupt):
-        return None
