d1153f5d6a9f1c896ab17e39882ce118dd4b3f8a
Yi Gu <yigu404@gmail.com>
2015-04-05 17:54:50 -0700

    Modified lambda.py

--- students/YIGU/session05/lambda.py
+++ students/YIGU/session05/lambda.py
@@ -1,10 +1,34 @@
-# This function returns a list of n functions, such that each one, when called,\
-# will return the input value, incremented by an increasing number.
+# This function returns a list of n functions, such that each one, when\
+# called, will return the input value, incremented by an increasing number.
+
+# failed number1
+"""def list_functions(n):
+    functions = [''] * n
+    for i in range(n):
+        functions[i] = lambda x: x+i
+    return functions"""
+
+# failed number2
+"""
+def list_functions(n):
+    functions = []
+    for i in range(n):
+        functions.append(lambda x: x+i)
+    return functions
+"""
+
+
+def var_factory(n):
+    return lambda x: x+n
 
 
 def list_functions(n):
-    functions = [lambda x:x] * n
-    print functions
+    functions = []
     for i in range(n):
-        functions[i] = lambda x: x+i
-        print functions[i]
+        functions.append(var_factory(i))
+    return functions
+
+
+functions = list_functions(5)
+for i in functions:
+    print i(2)
