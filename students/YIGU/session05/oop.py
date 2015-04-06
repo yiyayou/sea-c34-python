fa5272271859b68e795ffa3cc3053fe04af9c5d8
Yi Gu <yigu404@gmail.com>
2015-04-05 21:01:12 -0700

    Move classes and subclasses to session05

--- students/YIGU/session05/oop.py
+++ students/YIGU/session05/oop.py
@@ -3,66 +3,60 @@
 # 1. test 1 with same vars
 
 
-class father1(object):
-    def __init__(self):
-        self.eyes = 'blue'
-        self.hair = 'blond'
+def q1():
+    class father1(object):
+        def __init__(self):
+            self.eyes = 'blue'
+            self.hair = 'blond'
 
+    class mother1(object):
+        def __init__(self):
+            self.eyes = 'brown'
+            self.hair = 'black'
 
-class mother1(object):
-    def __init__(self):
-        self.eyes = 'brown'
-        self.hair = 'black'
+    class child1(father1, mother1):
+        def __init__(self):
+            # father1.__init__(self)
+            mother1.__init__(self)
+            father1.__init__(self)
 
+        def get_eye_color(self):
+            return self.eyes
 
-class child1(father1, mother1):
-    def __init__(self):
-        # father1.__init__(self)
-        mother1.__init__(self)
-        father1.__init__(self)
+        def get_hair_color(self):
+            return self.hair
 
-    def get_eye_color(self):
-        return self.eyes
-
-    def get_hair_color(self):
-        return self.hair
-
-f1 = father1()
-m1 = mother1()
-c1 = child1()
-print c1.get_eye_color()
-print c1.get_hair_color()
+    c1 = child1()
+    print c1.get_eye_color()
+    print c1.get_hair_color()
 
 # Answer: Yes. Inher from which ever come call last
 
 # 2. test 2 with diff vars
 
 
-class father2(object):
-    def __init__(self):
-        self.eyes = 'blue'
+def q2():
+    class father2(object):
+        def __init__(self):
+            self.eyes = 'blue'
 
+    class mother2(object):
+        def __init__(self):
+            self.hair = 'black'
 
-class mother2(object):
-    def __init__(self):
-        self.hair = 'black'
+    class child2(father2, mother2):
+        def __init__(self):
+            father2.__init__(self)
+            mother2.__init__(self)
 
+        def get_eye_color(self):
+            return self.eyes
 
-class child2(father2, mother2):
-    def __init__(self):
-        father2.__init__(self)
-        mother2.__init__(self)
+        def get_hair_color(self):
+            return self.hair
 
-    def get_eye_color(self):
-        return self.eyes
-
-    def get_hair_color(self):
-        return self.hair
-
-f2 = father2()
-m2 = mother2()
-c2 = child2()
-print c2.get_eye_color()
-print c2.get_hair_color()
+    c2 = child2()
+    print c2.get_eye_color()
+    print c2.get_hair_color()
 
 # Answer: Yes.
