71ad4d586abc4886fbe968eddee6cce11c0987e8
Yi Gu <yigu404@gmail.com>
2015-04-06 17:54:43 -0700

    Add function to testing.py

--- students/YIGU/session06/testing.py
+++ students/YIGU/session06/testing.py
@@ -3,24 +3,26 @@
 # in my_mod.py
 
 
-def my_func(val1, val2):
-    return val1 * val2
+def q():
+        def my_func(val1, val2):
+            return val1 * val2
 
-# in test_my_mod.py
-import unittest
+        # in test_my_mod.py
+        import unittest
 
 
-class MyFuncTestCase(unittest.TestCase):
-    def test_my_func(self):
-        test_vals = (2, 3)
-        expected = reduce(lambda x, y: x * y, test_vals)
-        # expected = reduce(lambda x, y: x + y, test_vals)
-        print expected
-        actual = my_func(*test_vals)
-        print actual
-        self.assertEquals(expected, actual)
+        class MyFuncTestCase(unittest.TestCase):
+            def test_my_func(self):
+                test_vals = (2, 3)
+                expected = reduce(lambda x, y: x * y, test_vals)
+                # expected = reduce(lambda x, y: x + y, test_vals)
+                print expected
+                actual = my_func(*test_vals)
+                print actual
+                self.assertEquals(expected, actual)
 
-if __name__ == '__main__':
-    unittest.main()
+        if __name__ == '__main__':
+            unittest.main()
 
+q()
 # Answer: Yes
