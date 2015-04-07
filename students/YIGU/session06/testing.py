f38abc2bdaeac6dd865a791b83487cabeebb1d17
Yi Gu <yigu404@gmail.com>
2015-04-06 17:45:31 -0700

    Add task18,session06 & testing.py

---
+++ students/YIGU/session06/testing.py
@@ -0,0 +1,26 @@
+# Testing: can I use unit test in the same file as my code?
+
+# in my_mod.py
+
+
+def my_func(val1, val2):
+    return val1 * val2
+
+# in test_my_mod.py
+import unittest
+
+
+class MyFuncTestCase(unittest.TestCase):
+    def test_my_func(self):
+        test_vals = (2, 3)
+        expected = reduce(lambda x, y: x * y, test_vals)
+        # expected = reduce(lambda x, y: x + y, test_vals)
+        print expected
+        actual = my_func(*test_vals)
+        print actual
+        self.assertEquals(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Answer: Yes
