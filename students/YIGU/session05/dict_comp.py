c330572cd1beb6c2cba449568d7c7e563836c177
Yi Gu <yigu404@gmail.com>
2015-04-12 18:42:55 -0700

    Add 5b to dict_comp.py

--- students/YIGU/session05/dict_comp.py
+++ students/YIGU/session05/dict_comp.py
@@ -9,8 +9,9 @@ food_prefs = {"name": u"Yi",
 
 # 1. Print the dict by passing it to a string format method
 
-print "{name} is from {city}, and she likes {cake} cake, {fruit} fruit,\
-    {salad} salad, and {pasta} pasta".format(**food_prefs)
+p = ("{name} is from {city}, and she likes {cake} cake, {fruit} fruit,"
+     "{salad} salad, and {pasta} pasta")
+print p.format(**food_prefs)
 
 # 2. build a dict of numbers from zero to fifteen & the hexadecimal equivalent
 
@@ -39,9 +40,17 @@ dict_q4 = {i: food_prefs[i].count(u'a') for i in food_prefs}
 print dict_q4
 # 5.
 
+# 5a
 s2 = {n for n in range(21) if (n % 2) == 0}
 s3 = {n for n in range(21) if (n % 3) == 0}
 s4 = {n for n in range(21) if (n % 4) == 0}
+
+print s2
+print s3
+print s4
+
+# 5b
+s2, s3, s4 = [{i for i in range(21) if not i % j} for j in range(2, 5)]
 
 print s2
 print s3
