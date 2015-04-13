29d4991ed470c44502a41dfc5620cb5e5e0e1f60
Yi Gu <yigu404@gmail.com>
2015-04-12 22:46:16 -0700

    Modified list_comp.py, remove old tests

--- students/YIGU/session07/list_comp.py
+++ students/YIGU/session07/list_comp.py
@@ -1,7 +1,2 @@
 def count_evens(nums):
     return len([i for i in nums if (i % 2) == 0])
-
-if __name__ == '__main__':
-    assert count_evens([2, 1, 2, 3, 4]) == 3
-    assert count_evens([2, 2, 0]) == 3
-    assert count_evens([1, 3, 5]) == 0
