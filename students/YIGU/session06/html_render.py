ea407fe9d27ac81cbca37d495bf947abd6675270
Yi Gu <yigu404@gmail.com>
2015-04-13 21:04:33 -0700

    Add Element class

--- students/YIGU/session06/html_render.py
+++ students/YIGU/session06/html_render.py
@@ -5,7 +5,31 @@ Python class example.
 
 """
 
-# The start of it all:
-# Fill it all in here.
+
 class Element(object):
+    """An HTML element."""
+    tag = u"html"
+    indent = u"    "
+
+    def __init__(self, content=None):
+        self.content = self.indent + str(self.content) if content else ""
+
+    def append(self, string):
+        """Append string to content."""
     pass
+
+    def render(self, file_out, ind=""):
+        """Render the tag and strings in content."""
+    pass
+
+
+class Html(Element):
+    """HTML tag."""
+
+    tag = u"html"
+
+
+class Body(Element):
+    """Body tag."""
+
+    tag = u"body"
